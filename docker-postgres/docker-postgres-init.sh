d1ceb68a818162644ff69cb27264d87810bb92a9
rajendarreddyj <rajendarreddyj@gmail.com>
2019-12-21 21:26:17 -0600

    port changes

--- docker-postgres/docker-postgres-init.sh
+++ docker-postgres/docker-postgres-init.sh
@@ -1,1 +1,1 @@
-docker run --name docker-postgres -v /apps/home/postgres/data:/var/lib/postgresql/data -e POSTGRES_PASSWORD=mysecretpassword -d postgres:latest
+docker run --name docker-postgres -v /apps/home/postgres/data:/var/lib/postgresql/data -p 5432:5432 -e POSTGRES_PASSWORD=mysecretpassword -d postgres:latest
